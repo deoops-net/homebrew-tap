3ffab7a3006ca3814f9dcf5781d75c3e7459f6cf
yuyang <yuyang@xiaoyun.com>
2019-07-24 17:02:23 +0800

    first commit

---
+++ dotam.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dotam < Formula
+  desc "a tool for pipline locally"
+  homepage ""
+  url "https://github.com/deoops-net/dotam/releases/download/v0.1.1/dotam_0.1.1_Darwin_x86_64.tar.gz"
+  sha256 "30ae7a977cd2a265660934e188876aec70b51d1971de951803878733565078c0"
+
+
+  def install
+    bin.install "dotam"
+  end
+end
