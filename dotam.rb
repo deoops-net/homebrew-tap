ca38dd07b3f6363a26dd392435add68865fc8d6a
dayu <dayuoba@users.noreply.github.com>
2019-08-01 11:35:31 +0800

    Update dotam.rb

--- dotam.rb
+++ dotam.rb
@@ -4,8 +4,8 @@
 class Dotam < Formula
   desc "a tool for pipline locally"
   homepage ""
-  url "https://github.com/deoops-net/dotam/releases/download/v0.1.41/dotam_0.1.41_Darwin_x86_64.tar.gz"
-  sha256 "dfe51a4baf09c437354c44386eb8b23f2cb6522a61df0930d9f425b69649f925"
+  url "https://github.com/deoops-net/dotam/releases/download/v0.1.44/dotam_0.1.44_Darwin_x86_64.tar.gz"
+  sha256 "3e7752d81eb248d6bdeeb28ed20ccdcd4647f8eb127b1954ee0ed60873932830"
 
   def install
     bin.install "dotam"
